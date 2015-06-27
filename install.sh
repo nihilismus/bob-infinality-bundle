27e5b97490b0a95965f2edb673becd31cd84e6d9
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2015-06-26 23:02:32 -0500

    Updates to fontconfig-infinality-ultimate, rev. 2.11.1-28

--- install.sh
+++ install.sh
@@ -9,6 +9,27 @@ rm -rf $TMP
 # Number of CPUs + 1
 export NUMJOBS=$(expr $(lscpu | grep -E '^CPU\(s\):' | xargs | cut -d ' ' -f 2) + 1)
 
+# Detect the installation of lxml
+LXMLPKG=$(ls /var/log/packages/lxml* 2>/dev/null || true)
+if [ -z "$LXMLPKG" ]; then
+  echo "###"
+  echo "### Error, lxml seems to be not installed."
+  echo "###"
+  echo "### You can find lxml's SlackBuild at:"
+  echo "###"
+  echo "###  http://slackbuilds.org/result/?search=lxml&sv="
+  echo "###"
+  echo "### Install it and execute this script one again."
+  echo "###"
+  exit 1
+fi
+
+LXMLPKG=$(basename $LXMLPKG)
+echo "###"
+echo "### lxml detected: $LXMLPKG"
+echo "###"
+sleep 3
+
 echo "###"
 echo "### Building: source/l/freetype ..."
 echo "###"
@@ -39,4 +60,6 @@ sleep 3
   upgradepkg --install-new --reinstall $TMP/cairo*.txz
 ) || exit 1
 
-echo "Finished."
+echo "###"
+echo "### Finished."
+echo "###"
